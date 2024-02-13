1b91967179732fbfd29638c1aa1532007f2b17e8
Mohammed Usama <usamabhakrani@gmail.com>
2024-02-13 14:36:36 +0500

    using zustand devtools

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,7 +2,8 @@ import LoginStatus from "./Auth/LoginStatus";
 import useCounterStore from "./Counter/store";
 
 const NavBar = () => {
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+  console.log("render nav");
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
